89404eb4f11c62487fca39b58529ac79acdc7dd5
Reino76 <76reino@gmail.com>
2026-02-09 12:55:52 +0200

    Name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://<username>.github.io',
+  site: 'https:Reino76.github.io',
   base: '/Kissakala-Wiki', // Explicitly set for your repo name
   integrations: [react(), tailwind()],
 });
